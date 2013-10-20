bbaf9ae59613d1d1e5d63094c73605b3e6fd4e19
Kevin Corina <batchman66@aol.com>
2013-10-19 23:00:56 -0400

    Create bgmenu.php

---
+++ bgmenu.php
@@ -0,0 +1,7 @@
+<ul class="bgmenu">
+   <li><a href="#" onclick="bgchgimg('1')">Default</a></li>
+   <li><a href="#" onclick="bgchgimg('2')">Cadillac</a></li>
+   <li><a href="#" onclick="bgchgimg('5')">East Beach</a></li>
+   <li><a href="#" onclick="bgchgimg('3')">High Falls</a></li>
+   <li><a href="#" onclick="bgchgimg('4')">Whiteface</a></li>  
+</ul>   
